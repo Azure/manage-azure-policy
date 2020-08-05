73b9420cf5ef6b40a46f400a275e7dac0537ae82
ajinkya599 <11447401+ajinkya599@users.noreply.github.com>
2020-08-05 10:32:26 +0530

    Scaffolded create/update Azure policies action.

---
+++ src/run.ts
@@ -0,0 +1,11 @@
+import * as core from '@actions/core';
+
+async function run() {
+  try {
+    // Action implementation goes here...
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
