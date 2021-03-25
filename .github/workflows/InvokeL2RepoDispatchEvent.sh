3e93d722a0806b407785847d8b16222da3840955
Jyotsna <Josh-01@github.com>
2021-03-25 16:36:32 +0530

    Update InvokeL2RepoDispatchEvent.sh

--- .github/workflows/InvokeL2RepoDispatchEvent.sh
+++ .github/workflows/InvokeL2RepoDispatchEvent.sh
@@ -22,7 +22,7 @@ getPayLoad() {
 EOF
 }
 
-response=$(curl -U "josh-01:$token" -X POST https://api.github.com/repos/Azure/azure-actions-integration-tests/dispatches --data "$(getPayLoad)")
+response=$(curl -u josh-01:$token -X POST https://api.github.com/repos/Azure/azure-actions-integration-tests/dispatches --data "$(getPayLoad)")
 
 if [ "$response" == "" ]; then
     echo "Integration tests triggered successfully"
